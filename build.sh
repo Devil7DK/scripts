acad04d72c8f96ad90891675531994cf5dfa2b1c
Devil7 <dineshthanhgavel47@gmail.com>
2018-03-03 09:25:26 +0000

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+ROM_NAME="${PWD##*/}" #Usually i use the rom name for its dir. So get name from there
+DEVICE_NAME=land
+export USE_CCACHE=1
+prebuilts/misc/linux-x86/ccache/ccache -M 100G
+export KBUILD_BUILD_USER=devil7
+export KBUILD_BUILD_HOST=TeamReloaded
+source build/envsetup.sh
+lunch "${ROM_NAME}_${DEVICE_NAME}-userdebug"
+case $ROM_NAME in
+	aicp|lineage|candy)time brunch  ${DEVICE_NAME} 2>&1 | tee build.log ;;
+	viper|aosvp)time mka poison 2>&1 | tee build.log ;;
+        *)time mka bacon 2>&1 | tee build.log ;;
+esac;
---
+++ envunch.sh
@@ -0,0 +1,7 @@
+ROM_NAME="${PWD##*/}" #Usually i use the rom name for its dir. So get name from there
+export USE_CCACHE=1
+prebuilts/misc/linux-x86/ccache/ccache -M 100G
+export KBUILD_BUILD_USER=devil7
+export KBUILD_BUILD_HOST=TeamReloaded
+source build/envsetup.sh
+lunch "${ROM_NAME}_land-userdebug"
