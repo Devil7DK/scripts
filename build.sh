6fc22c25c4e33981f032c6376d7c0153c9eb1d54
Devil7 <dineshthangavel47@gmail.com>
2018-03-28 14:06:27 +0000

    Add citrus

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ lunch "${ROM_NAME}_${DEVICE_NAME}-userdebug"
 case $ROM_NAME in
 	aicp|lineage|candy)time brunch  ${DEVICE_NAME} 2>&1 | tee build.log ;;
 	viper|aosvp)time mka poison 2>&1 | tee build.log ;;
+	citrus)time mka lemonade -j$(($(nproc)*2)) 2>&1 | tee build.log ;;
         *)time mka bacon 2>&1 | tee build.log ;;
 esac;
