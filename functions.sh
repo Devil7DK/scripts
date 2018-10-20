768e5d99a92ade6116465076d1428cbbe12e032d
Devil7DK <dineshthangavel47@gmail.com>
2018-10-20 12:57:52 +0000

    Add functions to setup and build kernels

--- functions.sh
+++ functions.sh
@@ -1,6 +1,9 @@
 script_path=$BASH_SOURCE
 script_dir=$(dirname "$script_path")
+
+# Import Scripts
 source $script_dir/colors.sh
+source $script_dir/toolchains.sh
 
 function show_banner() {
     echo -e "\n${BLUE}==================================================================================================================${NC}"
@@ -11,4 +14,113 @@ function show_banner() {
     echo -e "${BLUE}/_______  /\___  >\_/ |__|____/${DARKGRAY}____/   ${BLUE}        ${WHITE}/_______  /\____/|__|   |__|   \/\_/  (____  /__|    \___  >____  >${NC}"
     echo -e "${BLUE}        \/     \/              ${DARKGRAY}        ${BLUE}        ${WHITE}        \/                                 \/            \/     \/ ${NC}"
     echo -e "${BLUE}==================================================================================================================${NC}"
+}
+
+function setup() {
+    case $1 in
+        kernel-building)
+            setup-kernel-building
+        ;;
+        *)
+            echo -e $RED"Invalid Parameter."$NC
+        ;;
+    esac
+}
+
+function setup-kernel-building() {
+    echo -e $GREEN"Preparing Environment for Kernel Building..."$NC
+    echo -e $BLUE"Pick ARCH for building:"$YELLOW
+    select arch in arm arm64 ;
+    do
+        case "$arch" in
+            arm)
+                export ARCH=arm
+                export SUBARCH=arm
+                break
+                ;;
+            arm64)
+                export ARCH=arm64
+                export SUBARCH=arm64
+                break
+                ;;
+        esac
+    done
+    echo -e $GREEN"Selected Arch: $arch"$NC
+    echo ""
+
+    echo -e $BLUE"Pick toolchain for building:"$YELLOW
+    select tc in aosp-gcc aosp-clang ;
+    do
+        case "$tc" in
+            aosp-gcc)
+                setup-aosp-gcc
+                break
+                ;;
+            aosp-clang)
+                setup-aosp-gcc
+                setup-aosp-clang
+                break
+                ;;
+        esac
+    done
+    echo -e $NC
+    # Export Other Variables
+    export O=out/
+    export USE_CCACHE=1
+
+    mkdir -p out
+    echo -e $GREEN"Setup completed..."$NC
+}
+
+function setup-aosp-gcc() {
+    echo -e $GREEN"Setting up GCC for $arch architecture..."$NC
+    case $arch in
+        arm)
+            setup_toolchain $tc_aosp_gcc_arm_path $tc_aosp_gcc_arm $tc_aosp_branch
+            export CROSS_COMPILE=arm-linux-androideabi-
+            break
+            ;;
+        arm64)
+            setup_toolchain $tc_aosp_gcc_arm64_path $tc_aosp_gcc_arm64 $tc_aosp_branch
+            export CROSS_COMPILE=aarch64-linux-android-
+            break
+            ;;
+        *)
+            echo -e $RED"Unknown/Unsupported Arch! Aborting..."$NC && exit 1
+    esac
+}
+
+function setup-aosp-clang() {
+    echo -e $GREEN"Setting up clang..."$NC
+    setup_toolchain $tc_aosp_clang_path $tc_aosp_clang $tc_aosp_branch
+    export CC="$tc_aosp_clang_path/bin/clang"
+    export CLANG_TRIPLE=aarch64-linux-gnu-
+}
+
+# Check Path & Clone Toolchain if it doesn't exist
+function setup_toolchain() {
+    path=$1
+    url=$2
+    branch=$3
+
+    if [ ! -d "$path" ]; then
+        parent=$(dirname $path)
+        mkdir -p "$parent"
+        echo -e $GREEN"Cloning toolchain..."$NC
+        git clone --depth=2 $url -b $branch $path > /dev/null 2>&1 || (echo -e $RED"Unable to clone toolchain! Aborting..."$NC && exit 1)
+    fi
+
+    export PATH=$PATH:$path/bin
+}
+
+# make_kernel <device_name> <additional_parms>
+function make_kernel() {
+    device=$1
+    add_parms=$2
+
+    echo -e $GREEN"Building for $device"$NC
+    make mrproper &&
+    make clean &&
+    make ${device}_defconfig &&
+    time make -j$(($(nproc)*2)) $add_parms 2>&1 | tee build.log
 }
---
+++ toolchains.sh
@@ -0,0 +1,14 @@
+# Common
+toolchains_path=$HOME/.toolchains
+
+# AOSP/Google
+tc_aosp_branch=android-9.0.0_r12
+
+tc_aosp_gcc_arm64=https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+tc_aosp_gcc_arm64_path=$toolchains_path/gcc/google/arm64
+
+tc_aosp_gcc_arm=https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/
+tc_aosp_gcc_arm_path=$toolchains_path/gcc/google/arm
+
+tc_aosp_clang=https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/
+tc_aosp_clang_path=$toolchains_path/clang/google
